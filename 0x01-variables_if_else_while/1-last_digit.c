fadb1a069fdd40fa47ad2b191328799e8166762a
betheldiddy <bethel.betlino@gmail.com>
2023-02-20 11:43:41 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,10 +23,8 @@ int main(void)
 		printf("Last digit of %d is %d is 0", n, m);
 	else if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and is not 0", n, m);
+
 	printf("\n");
 
 	return (0);
 }
-
-
-
