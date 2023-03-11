27aab3b50583a5480799296cf014535dddd3bf91
betheldiddy <bethel.betlino@gmail.com>
2023-03-11 15:32:28 -0800

    update

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,8 @@
-#include "main.h"
-
+#include "main.h
+/**
+*return 0
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+_puts("\"At the end of the day, i want to be the best hacker\"\n\t- bethel");
+	return (0);
 }
