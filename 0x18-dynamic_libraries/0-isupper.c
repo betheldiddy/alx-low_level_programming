07b52f62ea3fd9520dc7b7d260a610df9d10578a
“betheldiddy” <bethel.betlino@gmai.com>
2023-08-24 14:30:19 -0700

    this ia another one and there is room for improvement

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isupper - check for the uppercase characters
+* @c: Variable text
+* Return: Alway 0.
+*/
+int _isupper(int c)
+{
+
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+This is my readme file on the static and  dynamic library project
